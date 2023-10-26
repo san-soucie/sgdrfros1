89a71e3fc762a6a5c9c3da670cbcf584e75381c1
John San Soucie <jsansoucie@whoi.edu>
2023-10-26 15:45:37 +0000

    more printing 2

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cat /docker-ros/ws/.install-dependencies.sh
-find /docker-ros/ws/src/ -type f -name '*PhinsConfig.cfg' -exec echo "{}" \; -exec sed -i 's/"Phins"/"PhinsConfig"/' {} \;
+cat ${WORKSPACE}/.install-dependencies.sh
+find src/upstream -type f -name '*PhinsConfig.cfg' -exec echo "{}" \; -exec sed -i 's/"Phins"/"PhinsConfig"/' {} \;

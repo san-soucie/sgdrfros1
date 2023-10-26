e3109f3703e69798334c2a68f63459c29545d49a
John San Soucie <jsansoucie@whoi.edu>
2023-10-26 16:19:19 +0000

    bleh

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,4 +1,1 @@
 #!/usr/bin/env bash
-find ${WORKSPACE} -type f
-cat ${WORKSPACE}/.install-dependencies.sh
-find src/upstream -type f -name '*PhinsConfig.cfg' -exec echo "{}" \; -exec sed -i 's/"Phins"/"PhinsConfig"/' {} \;
